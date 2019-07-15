97c323b09b09ca065a23f7ad3ec3fa2790d5a3e4
work mel <indemnedev@gmail.com>
2019-07-15 14:36:44 +0400

    Updated Media Uploader

--- x_moikzz_application/helpers/utilities_helper.php
+++ x_moikzz_application/helpers/utilities_helper.php
@@ -1,11 +1,148 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-	// @mel
-	if (!function_exists('file_front_dir')) {
+	/**
+	 * @mel 
+	 */	
+	if (!function_exists('file_common_dir')) {
 		function file_common_dir( $file ){ 
 			return  base_url().'x_moikzz_assets/'.$file; 
 		} 
-	} 
+	}
+
+	/**
+	 * Media Uploader
+	 */
+	if (!function_exists('media_uploader')) {
+		function media_uploader(){ ?>
+			<!-- sample modal content -->
+			<div id="galMediaUploader" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="vcenter" aria-hidden="true">
+				<div class="modal-dialog modal-dialog-centered modal-xl">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h4 class="modal-title" id="vcenter">Media Uploader</h4>
+							<button id="closeModal" type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+						</div>
+						<div class="modal-body">
+
+<?php 
+/**
+ * Media Uplaoder Container
+ */
+?>
+<!-- Media Uploader -->
+<div class="col-12 p-0">
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs customtab2 border-light" role="tablist">
+        <li class="nav-item"> <a class="nav-link show bg-light text-secondary active" data-toggle="tab" href="#gallerymedia" role="tab" aria-selected="true"><span class="sm-up"><i class="mdi mdi-camera"></i></span> <span class="hidden-xs-down">Media Gallery</span></a></li>
+        <li class="nav-item"> <a class="nav-link show bg-light text-secondary" data-toggle="tab" href="#uploadmedia" role="tab" aria-selected="false"><span class="sm-up"><i class="mdi mdi-cloud-upload"></i></span> <span class="hidden-xs-down">Upload</span></a></li>
+    </ul>
+
+    <!-- Tab panes -->
+    <div class="tab-content media-uploader">
+        <div class="tab-pane show active" id="gallerymedia" role="tabpanel">
+            <div class="card mb-0">
+                <div class="card-body">
+
+                    <div class="col-md-12">
+                        <div class="card-block">
+                            <div class="row">                                       
+                                <div class="col-8">
+                                    <div class="row">
+                                        <h4 class="card-title" style="width:100%;">Media Gallery</h4>
+                                        <h6 class="card-subtitle">Select the file and click choose to use the file.</b></h6>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="row pull-right">
+                                        <div id="selectBtn"></div>
+                                    </div>
+                                </div>
+                                <div class="col-md-9 border border-right-0">
+                                    <div class="row">
+                                        <div class="media-files-container p-2 gl-flex gl-wrap">
+                                            <div class="spinner-border" role="status"></div>
+                                        </div>
+                                        <div class="gal-load-more-container">
+                                            <button type="button" id="loadMoreItems" class="shadow-none btn-sm waves-effect waves-light btn-secondary">Load More <i class="mdi mdi-arrow-down-bold-circle"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 border p-3 media-file-details">
+                                    <form action="">
+                                        <input id="mID" type="hidden" value="">
+                                        <div class="form-group gal-selected-img-container text-center border">
+                                            <img id="mImage" class="img-fluid" src="<?php echo file_common_dir('images/default.png');?>" alt="Gallega Image Preview">
+                                        </div>
+                                        <div class="form-group mb-2 row">
+                                            <label for="example-text-input" class="col-3 col-form-label"><small>Title</small></label>
+                                            <div class="col-9">
+                                                <input id="mTitle" class="form-control form-control-sm" type="text" value="" id="mediaID">
+                                            </div>
+                                        </div>
+                                        <div class="form-group mb-2 row">
+                                            <label for="example-text-input" class="col-3 col-form-label"><small>Alt</small></label>
+                                            <div class="col-9">
+                                                <input id="mAlt" class="form-control form-control-sm" type="text" value="" id="mediaALT">
+                                            </div>
+                                        </div>
+                                        <div class="form-group mb-2 row">
+                                            <div class="col-12 text-right">
+												<a href="#" class="pull-left text-secondary"><small>View in new tab</small></a>
+                                                <button type="button" class="btn waves-effect waves-light btn-sm btn-secondary">Update</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div><!-- Card -->
+        </div>
+    
+        <div class="tab-pane" id="uploadmedia" role="tabpanel">
+            <div class="card mb-0">
+                <div class="card-body">
+                    <h4 class="card-title">Media Uploader</h4>
+                    <h6 class="card-subtitle">Select files or drag and drop the files to upload.</h6>
+                    <form action="#" class="dropzone">
+                        <div class="fallback">
+                            <input name="file" type="file" multiple />
+                        </div>
+                    </form>
+                </div>
+            </div><!-- card -->
+        </div>
+
+    </div><!-- tab-content -->
+</div><!-- Media Uploader -->
+							
+
+
+							<?php //$this->load->view('templates/inc/back/media_uploader.html'); ?>
+						</div>
+						<!-- <div id="selectBtn" class="modal-footer"></div> -->
+					</div>
+					<!-- /.modal-content -->
+				</div>
+				<!-- /.modal-dialog -->
+			</div>
+			<!-- /.modal -->
+			<!-- <div class="from-group gal-media-uploader">
+				<input class="modal-media-url form-control" type="text">
+				<button id="openMediaModal" class="btn waves-effect waves-light btn-secondary" data-toggle="modal" data-target="#galMediaUploader">Media Uploader</button>
+			</div> -->
+
+		<?php
+			// return base_url().'x_moikzz_assets/'.$file; 
+		} 
+	}
+
+
+
+	/* Media Uploader */
+
 	
 	if (!function_exists('file_common_dir_back')) { 
 
--- x_moikzz_application/views/templates/contents/back/media.php
+++ x_moikzz_application/views/templates/contents/back/media.php
@@ -1,8 +1,105 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <div class="row">
+    
+    <!-- <div class="form-group gal-selected-img-container text-center border">
+       
+    </div> -->
+    <!-- <div class="from-group gal-media-uploader">
+        <img id="mImage" class="img-fluid" src="<?php echo file_common_dir('images/default.png');?>" alt="Gallega Image Preview">
+        <input class="modal-media-url form-control" type="text">
+        <button type="button" id="openMediaModal" class="btn waves-effect waves-light btn-secondary" data-toggle="modal" data-target="#galMediaUploader">Media Uploader</button>
+    </div> -->
 
     <?php //$this->load->view('templates/inc/back/media_modal.html'); ?>
     
-    <?php $this->load->view('templates/inc/back/media_uploader.html'); ?>
+    <?php //$this->load->view('templates/inc/back/media_uploader.html'); ?>
+
+    <!-- Media Uploader -->
+<div class="col-12 p-0">
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs customtab2 border-light" role="tablist">
+        <li class="nav-item"> <a class="nav-link show bg-light text-secondary active" data-toggle="tab" href="#gallerymedia" role="tab" aria-selected="true"><span class="sm-up"><i class="mdi mdi-camera"></i></span> <span class="hidden-xs-down">Media Gallery</span></a></li>
+        <li class="nav-item"> <a class="nav-link show bg-light text-secondary" data-toggle="tab" href="#uploadmedia" role="tab" aria-selected="false"><span class="sm-up"><i class="mdi mdi-cloud-upload"></i></span> <span class="hidden-xs-down">Upload</span></a></li>
+    </ul>
+
+    <!-- Tab panes -->
+    <div class="tab-content media-uploader">
+        <div class="tab-pane show active" id="gallerymedia" role="tabpanel">
+            <div class="card mb-0">
+                <div class="card-body">
+
+                    <div class="col-md-12">
+                        <div class="card-block">
+                            <div class="row">                                       
+                                <div class="col-8">
+                                    <div class="row">
+                                        <h4 class="card-title" style="width:100%;">Media Gallery</h4>
+                                        <h6 class="card-subtitle">Select the file and click choose to use the file.</b></h6>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="row pull-right">
+                                        <div id="selectBtn"></div>
+                                    </div>
+                                </div>
+                                <div class="col-md-9 border border-right-0">
+                                    <div class="row">
+                                        <div class="media-files-container p-2 gl-flex gl-wrap">
+                                            <div class="spinner-border" role="status"></div>
+                                        </div>
+                                        <div class="gal-load-more-container">
+                                            <button id="loadMoreItems" class="shadow-none btn-sm waves-effect waves-light btn-secondary">Load More <i class="mdi mdi-arrow-down-bold-circle"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 border p-3 media-file-details">
+                                    <form action="">
+                                        <input id="mID" type="hidden" value="">
+                                        <div class="form-group gal-selected-img-container text-center border">
+                                            <img id="mImage" class="img-fluid" src="<?php echo file_common_dir('images/default.png');?>" alt="Gallega Image Preview">
+                                        </div>
+                                        <div class="form-group mb-2 row">
+                                            <label for="example-text-input" class="col-3 col-form-label"><small>Title</small></label>
+                                            <div class="col-9">
+                                                <input id="mTitle" class="form-control form-control-sm" type="text" value="" id="mediaID">
+                                            </div>
+                                        </div>
+                                        <div class="form-group mb-2 row">
+                                            <label for="example-text-input" class="col-3 col-form-label"><small>Alt</small></label>
+                                            <div class="col-9">
+                                                <input id="mAlt" class="form-control form-control-sm" type="text" value="" id="mediaALT">
+                                            </div>
+                                        </div>
+                                        <div class="form-group mb-2 row">
+                                            <div class="col-12 text-right">
+                                                <button type="button" class="btn waves-effect waves-light btn-sm btn-secondary">Update</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div><!-- Card -->
+        </div>
+    
+        <div class="tab-pane" id="uploadmedia" role="tabpanel">
+            <div class="card mb-0">
+                <div class="card-body">
+                    <h4 class="card-title">Media Uploader</h4>
+                    <h6 class="card-subtitle">Select files or drag and drop the files to upload.</h6>
+                    <form action="#" class="dropzone">
+                        <div class="fallback">
+                            <input name="file" type="file" multiple />
+                        </div>
+                    </form>
+                </div>
+            </div><!-- card -->
+        </div>
+
+    </div><!-- tab-content -->
+</div><!-- Media Uploader -->
 
 </div><!-- row -->
